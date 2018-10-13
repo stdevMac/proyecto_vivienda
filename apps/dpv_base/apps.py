e4f2f92601eb3439f669bc685d84cc77482dcc86
Gogs <gogs@fake.local>
2018-10-12 20:19:24 -0400

    Cambio de Django Admin a Django, puesta la plantilla, creado el esquema de los modulos/apps y otros

---
+++ apps/dpv_base/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class DpvBaseConfig(AppConfig):
+    name = 'apps.dpv_base'     # Nombre identificador del modulo tiene que cumplir con el estandar
+    verbose_name = 'Base DPV'  # Nombre de Humanizado del modulo sera utilizado para mostrar en el menu
+    menu = 'layouts/menu/main_menu.html'  # Plantilla HTML de que se mostrara el modulo del menu
+    menuable = False    # Si el modulo se mostrara en el menu
+    active = True       # Si el modulo esta activo, es como si esta instalado
+    parent = True      # Si el modulo es un submodulo de otro en el menu (y solo en el menu)
+    child_of = ''     # Si el modulo es hijo de otro en el menu se coloca aqui el nombre(atributo name) del modulo padre
