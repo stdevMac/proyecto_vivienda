5ded724ef6b69ab249ef1acbc37f5450233e0421
janet_dev <janet@oc.dpvhab.gob.cu>
2018-11-16 17:23:55 -0500

    Logramos qque saliera el modal del nomenclador provincia

--- apps/dpv_nomencladores/urls.py
+++ apps/dpv_nomencladores/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import index, index_calle, index_concepto, index_destino, index_genero, index_municipio, index_organismo, \
-                   index_piso, index_provincia, index_areatrabajo, index_centrotrabajo
+                   index_piso, index_provincia, index_areatrabajo, index_centrotrabajo, ProvinciaCreate, ProvinciaUpdate
 
 urlpatterns = [
     path('', index, name='nomenclador_index'),
     path('provincia/', index_provincia, name='nomenclador_provincia'),
+    path('crear_provincia/', ProvinciaCreate.as_view(), name='provincia_new'),
+    path('editar_provincia/<int:pk>/', ProvinciaUpdate.as_view(), name='provincia_edit'),
     path('municipio/', index_municipio, name='nomenclador_municipio'),
     path('calle/', index_calle, name='nomenclador_calle'),
     path('piso/', index_piso, name='nomenclador_piso'),
