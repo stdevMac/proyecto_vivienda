9384aa1c103b74f211e76e02cd8a5a28907795bd
Marcos Maceo <marcosmaceowork@gmail.com>
2019-03-18 10:36:52 -0400

    Added natural complaint in pedro's way

--- apps/dpv_complaint/render_form/render_forms.py
+++ apps/dpv_complaint/render_form/render_forms.py
@@ -5,35 +5,66 @@ from apps.dpv_complaint.models import *
 from apps.dpv_persona.forms import PersonaNaturalForm, PersonaJuridicaForm
 
 
-def check_person(form_person):
-    if form_person.is_valid():
-        return True
-    # Fix here problems of person_validation
-    return False
+def check_natural_person(ci, email_address):
+    # Check if exist a person for the CI
+    by_ci = PersonaNatural.objects.filter(ci=ci)
+    by_email = PersonaNatural.objects.filter(email_address=email_address)
+    return by_ci.exists() or by_email.exists()
 
 
-def form_natural_complaint(request):
-    form_name = "Queja de persona Natural"
+def form_natural_complaint(request, person_id):
+    form_name = "Datos de Queja Natural"
     if request.method == "POST":
         form_complaint = ComplaintForm(request.POST)
-        form_natural = PersonaNaturalForm(request.POST)
-        if form_complaint.is_valid() and check_person(form_natural):
+        if form_complaint.is_valid():
 
             complaint = form_complaint.save(commit=False)
             complaint.is_natural = True
-            complaint.person_natural = form_natural.save()
+            complaint.person_natural = PersonaNatural.objects.get(id=person_id)
             complaint.enter_date = timezone.now()
             complaint.save()
             p = WaitingForDistribution()
             p.complaint = complaint
             p.enterDate = timezone.now()
             p.save()
-            return redirect(reverse_lazy('index_natural_complaint'))
+            return redirect(reverse_lazy('get_complaints_for_person', args=[person_id]))
     else:
         form_complaint = ComplaintForm()
+    return render(request, "dpv_complaint/single_form.html",
+                  {'form': form_complaint, 'form_name': form_name})
+
+
+def middle_form_natural_complaint(request, person_id):
+    complaints = Complaint.objects.all().filter(person_natural=person_id)
+    if complaints.exists():
+        return render(request, "dpv_complaint/index_by_person.html", {'index': complaints,
+                                                                      'index_name': 'Obtener Persona por ID'})
+    else:
+        return redirect(reverse_lazy('add_natural_complaint', args=[person_id]))
+    pass
+
+
+def form_person_for_complaint(request):
+    form_name = "Datos de persona Natural"
+    if request.method == "POST":
+        form_natural = PersonaNaturalForm(request.POST)
+        email = form_natural.data.get('email_address')
+        ci = form_natural, form_natural.data.get('ci')
+        if check_natural_person(ci, email):
+            if email:
+                person = PersonaNatural.objects.get(email_address=email)
+            else:
+                person = PersonaNatural.objects.get(email_address=email)
+            return redirect(reverse_lazy('get_complaints_for_person', args=[person.id]))
+
+        elif form_natural.is_valid():
+            person = form_natural.save()
+            return redirect(reverse_lazy('add_natural_complaint', args=[person.id]))
+
+    else:
         form_natural = PersonaNaturalForm()
-    return render(request, "dpv_complaint/multiform_complaint.html",
-                  {'form_one': form_complaint, 'form_two': form_natural, 'form_name': form_name})
+    return render(request, "dpv_complaint/single_form.html",
+                  {'form': form_natural, 'form_name': form_name})
 
 
 def form_juridic_complaint(request):
